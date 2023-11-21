dktwho/additional-action-creator-thunk-axios-request-thunk-creator

add types hints

src/features/decks/DecksList/DeckItem/DeckItem.tsx
@@ -4,7 +4,7 @@ type DeckProps = {
   deck: any // todo: fix
 }
 
-const TEST_ACC_NAME = 'test acc'
+const TEST_ACC_NAME = 'v1det'
 
 export const DeckItem = ({ deck }: DeckProps) => {
   const isTestingDeck = deck.author.name === TEST_ACC_NAME

StarkovSergey/04-sprint-01-add-lesson-for-students

src/features/decks/DecksList/DeckItem/DeckItem.tsx
@@ -4,7 +4,7 @@ type DeckProps = {
   deck: any // todo: fix
 }
 
-const TEST_ACC_NAME = 'test acc'
+const TEST_ACC_NAME = 'testName'
 
 export const DeckItem = ({ deck }: DeckProps) => {
   const isTestingDeck = deck.author.name === TEST_ACC_NAME